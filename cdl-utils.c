f9284701dbfbe6abadb07ec21b1ff1e8a9c1bd16
Leonardo Amaro <35879142+leoamaro01@users.noreply.github.com>
2023-04-05 14:13:41 -0400

    Implemented splitstr

--- cdl-utils.c
+++ cdl-utils.c
@@ -1,4 +1,5 @@
 #include <dirent.h>
+#include <stdlib.h>
 #include "cdl-utils.h"
 
 bool is_valid_directory(char *dir)
@@ -11,17 +12,51 @@ bool is_valid_directory(char *dir)
 
 struct JaggedCharArray splitstr(char *str, char sep)
 {
-    int count;
+    int strLength = strlen(str);
+    int count = 0;
     int i;
+    int tokenPointer = 0;
 
-    for(i = 0; ; i++)
+    char **ret = malloc(strLength * sizeof(char *));
+
+    for (i = 0; i < strLength; i++)
     {
-        if(str[i] == '\0')
-            break;
-        
-        if(str[i] == sep)
+
+        if (str[i] != sep)
+            continue;
+
+        int len = i - tokenPointer;
+        size_t size = len * sizeof(char);
+        if (len == 0)
         {
-            
+            tokenPointer = i + 1;
+            continue;
         }
+
+        ret[count] = malloc(size);
+
+        memcpy(ret[count], str + tokenPointer, size);
+
+        count++;
+        tokenPointer = i + 1;
     }
+
+    if (strLength > tokenPointer)
+    {
+        int len = strLength - tokenPointer;
+        size_t size = len * sizeof(char);
+
+        ret[count] = malloc(size);
+
+        memcpy(ret[count], str + tokenPointer, size);
+        count++;
+    }
+    size_t size = count * sizeof(char *);
+    char **result = malloc(size);
+    memcpy(ret, result, size);
+
+    free(ret);
+
+    struct JaggedCharArray jaggedArr = {result, count};
+    return jaggedArr;
 }
